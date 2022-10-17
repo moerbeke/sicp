7a9eb79ce14a68adb86e3d048f4bcd205cfe0959
moerbeke <aceballos@gmail.com>
2022-10-17 19:19:11 +0200

    Solution to exercise 2.2

---
+++ 2.02.scm
@@ -0,0 +1,31 @@
+(define (make-segment p1 p2)
+  (cons (make-point (car p1) (cdr p1))
+        (make-point (car p2) (cdr p2))))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (midpoint-segment s)
+  (define (average a b) (/ (+ a b) 2))
+  (make-point (average (x-point (start-segment s)) (x-point (end-segment s)))
+              (average (y-point (start-segment s)) (y-point (end-segment s)))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
