23ac3139d061ce8cc8541f7f819bee160bc0a019
moerbeke <aceballos@gmail.com>
2023-06-10 11:41:48 +0200

    Apply the substitution model

--- 2.04.scm
+++ 2.04.scm
@@ -7,3 +7,12 @@
 (define (cdr z)
   (z (lambda (p q) q)))
 
+;(define p1 (cons 1 2))
+;(define p1 (lambda (m) (m 1 2)))
+;p1 <- (lambda (m) (m 1 2)))
+;
+;(car p1)
+;(car (lambda (m) (m 1 2)))
+;((lambda (m) (m 1 2)) (lambda (p q) p))
+;((lambda (p q) p) 1 2)
+;1
