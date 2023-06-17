504fe24030d1d4258c26dc31248d16c6eb98f9a3
moerbeke <aceballos@gmail.com>
2023-06-17 12:50:12 +0200

    Solution to exercise 2.7

--- 2.sicp.scm
+++ 2.sicp.scm
@@ -1,5 +1,7 @@
 ; SICP
 
+; 2.1.1 Example: Arithmetic Operations for Rational Numbers
+
 (define (add-rat x y)
   (make-rat (+ (* (numer x) (denom y))
                (* (numer y) (denom x)))
@@ -37,3 +39,22 @@
   (display (numer x))
   (display "/")
   (display (denom x)))
+
+; 2.1.4 Extended Exercise: Interval Arithmetic
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
